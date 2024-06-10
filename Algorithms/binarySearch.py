33eb292c66faeb45052ff613937d270f9676a8d7
Raul Bazan <47358801+RaulBSanchez@users.noreply.github.com>
2024-06-10 13:12:42 -0400

    fixed some variables

--- Algorithms/binarySearch.py
+++ Algorithms/binarySearch.py
@@ -1,24 +1,24 @@
-def binary_search(arr1, target):
+
+def binary_search(temp, target):
 	first_index = 0
-	end_index = len(arr) - 1
+	end_index = len(temp) - 1
 
 	while first_index <= end_index:
-		mid_point = first_index + (end_index - first_index) //2
-		mid_point_value = arr[mid_point]
+		mid_point = first_index + (end_index - first_index) // 2
+		mid_point_value = temp[mid_point]
 
 		if mid_point_value == target:
 			return mid_point
 
-		elif item < mid_point_value:
+		elif target < mid_point_value:
 			end_index = mid_point - 1
 
 		else:
-			begin_index = mid_point + 1
+			first_index = mid_point + 1
 
 	return None
 
+temp = [2,4,5,6,7,8,9,10,12,13,14,19,25,91]
+target = 91
+print(binary_search(temp, target))
 
-arr = [1, 3, 10, 15, 18, 20, 30, 49]
-target = 20
-
-print(binary_search(arr,target))
