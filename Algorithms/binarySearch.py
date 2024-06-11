97c0427944436cc962a72e6a9b1f957bfdaaeeb2
Raul Bazan <47358801+RaulBSanchez@users.noreply.github.com>
2024-06-11 14:56:08 -0400

    finishd the algorithm

--- Algorithms/binarySearch.py
+++ Algorithms/binarySearch.py
@@ -1,24 +1,28 @@
 
-def binary_search(temp, target):
+def binary_search(arr, target):
 	first_index = 0
-	end_index = len(temp) - 1
-
+	end_index = len(arr) - 1
+	mid_point = 0
+	mid_point_value = 0
+	
 	while first_index <= end_index:
 		mid_point = first_index + (end_index - first_index) // 2
-		mid_point_value = temp[mid_point]
-
+		mid_point_value = arr[mid_point]
+	
 		if mid_point_value == target:
 			return mid_point
 
 		elif target < mid_point_value:
 			end_index = mid_point - 1
+			
 
 		else:
 			first_index = mid_point + 1
+			
 
 	return None
 
-temp = [2,4,5,6,7,8,9,10,12,13,14,19,25,91]
-target = 91
-print(binary_search(temp, target))
+arr = [5, 6, 7, 18, 20, 25, 20]
+target = 20
+print(binary_search(arr, target))
 
